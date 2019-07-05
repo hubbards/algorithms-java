9bc6c2b7161f40ec80c875b45bda4a97ca8f7725
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-07-05 13:24:51 -0700

    Solve weighted interval scheduling problem

---
+++ src/main/java/com/github/hubbards/algorithms/schedule/WeightedIntervalRequest.java
@@ -0,0 +1,48 @@
+package com.github.hubbards.algorithms.schedule;
+
+import java.time.Instant;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * WeightedIntervalRequest represents an weighted request to use a resource for
+ * an interval of time.
+ *
+ * Two requests are compatible if their intervals don't overlap. Compatible
+ * requests can be scheduled on the same resource.
+ *
+ * @author Spencer Hubbard
+ */
+public class WeightedIntervalRequest extends IntervalRequest {
+    private int weight;
+
+    /**
+     * Constructs a new weighted interval request with a name, start time,
+     * finish time, and weight.
+     *
+     * @param name name of the request.
+     * @param start start time of the request.
+     * @param finish finish time of the request.
+     * @param weight weight of the request.
+     *
+     * @throws NullPointerException if any parameter is null.
+     * @throws IllegalArgumentException if start time is greater than or equal
+     * to finish time or weight is negative.
+     */
+    public WeightedIntervalRequest(String name, Instant start, Instant finish, int weight) {
+        super(name, start, finish);
+
+        checkArgument(weight >= 0, "weight must be non-negative");
+
+        this.weight = weight;
+    }
+
+    /**
+     * Getter for weight.
+     *
+     * @return weight of this request.
+     */
+    public int getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/com/github/hubbards/algorithms/schedule/WeightedIntervalSchedule.java
@@ -0,0 +1,134 @@
+package com.github.hubbards.algorithms.schedule;
+
+import com.google.common.collect.Iterators;
+import com.google.common.collect.PeekingIterator;
+
+import java.util.*;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * WeightedIntervalSchedule represents a solution to the weighted interval
+ * scheduling problem: given a collection of interval requests, schedule a
+ * subset of requests for a single resource with maximum total weight.
+ * <p>
+ * A solution is found using a dynamic programming algorithm.
+ *
+ * @author Spencer Hubbard
+ */
+public class WeightedIntervalSchedule {
+    private WeightedIntervalRequest[] requests;
+
+    // Memoized solutions to sub-problems.
+    private int[] memo;
+    private int totalWeight;
+
+    // TODO: replace with bottom-up construction
+    // Dynamic programming algorithm for weighted interval scheduling problem.
+    private WeightedIntervalSchedule(WeightedIntervalRequest[] requests) {
+        this.requests = requests;
+
+        memo = new int[requests.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = -1;
+        }
+
+        totalWeight = computeOpt(memo.length - 1);
+    }
+
+    // TODO: replace with bottom-up construction
+    private int computeOpt(int i) {
+        if (i < 0) {
+            return 0;
+        }
+
+        if (memo[i] == -1) {
+            WeightedIntervalRequest request = requests[i];
+            int j = i - 1;
+            while (j >= 0 && request.getStart().compareTo(requests[j].getFinish()) < 0) {
+                j--;
+            }
+            memo[i] = Math.max(request.getWeight() + computeOpt(j), computeOpt(i - 1));
+        }
+
+        return memo[i];
+    }
+
+    // TODO: document
+    public int totalWeight() {
+        return totalWeight;
+    }
+
+    // TODO: document
+    public int size() {
+        // TODO: implement
+        throw new RuntimeException("not implemented");
+    }
+
+    // TODO: document
+    public boolean contains(WeightedIntervalRequest request) {
+        checkNotNull(request);
+
+        // TODO: implement
+        throw new RuntimeException("not implemented");
+    }
+
+    /**
+     * Builder is a builder for an instance of the weighted interval
+     * scheduling problem.
+     */
+    public static class Builder {
+        private PriorityQueue<WeightedIntervalRequest> requests;
+
+        /**
+         * Constructs a new builder.
+         */
+        public Builder() {
+            requests = new PriorityQueue<WeightedIntervalRequest>(new FinishOrder());
+        }
+
+        /**
+         * Checks if this builder contains a given request.
+         *
+         * @param request the request to check for.
+         *
+         * @return <code>true</code> if this builder contains the given request,
+         * <code>false</code> otherwise.
+         *
+         * @throws NullPointerException if the given request is null.
+         */
+        public boolean hasRequest(WeightedIntervalRequest request) {
+            checkNotNull(request);
+
+            return requests.contains(request);
+        }
+
+        /**
+         * Adds a given request to this builder.
+         *
+         * @param request the request to add.
+         *
+         * @throws NullPointerException if the given request is null.
+         * @throws IllegalArgumentException if the given request has already
+         * been added to this builder.
+         */
+        public void addRequest(WeightedIntervalRequest request) {
+            checkNotNull(request);
+            checkArgument(!requests.contains(request), "request already added");
+
+            requests.add(request);
+        }
+
+        /**
+         * Builds a solution to the instance of the weighted interval scheduling
+         * problem represented by this builder.
+         *
+         * @return a solution to the instance of the weighted interval
+         * scheduling problem.
+         */
+        public WeightedIntervalSchedule build() {
+            return new WeightedIntervalSchedule((WeightedIntervalRequest[]) requests.toArray());
+        }
+    }
+}
